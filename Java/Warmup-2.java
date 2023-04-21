908d352708afff434ad55dc7b21ffe1053a297da
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2023-04-21 11:18:06 -0700

    Add countXX

--- Java/Warmup-2.java
+++ Java/Warmup-2.java
@@ -26,6 +26,18 @@ public String frontTimes(String str, int n)
 	return stbuilder.toString();
 }
 
+//Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+//countXX("abcxx") → 1
+//countXX("xxx") → 2
+//countXX("xxxx") → 3
+int countXX(String str) {
+  int count = 0;
+  for (int i = 0; i < str.length()-1; i++) {
+    if (str.substring(i, i+2).equals("xx")) count++;
+  }
+  return count;
+}
+
 
 // Given a string, return a new string made of every other char starting with the first,
 // so "Hello" yields "Hlo". 
